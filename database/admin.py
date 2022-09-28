68e1df761306416ef5863647d20b3a67a6618757
korchizhinskiy <korchizhinskiy@gmail.com>
2022-09-28 12:20:18 +0300

    Add models into admin's models

--- database/admin.py
+++ database/admin.py
@@ -1,3 +1,61 @@
 from django.contrib import admin
+from .models import Doctor, \
+                   Patient, \
+                   Department, \
+                   Specialization, \
+                   Chart, \
+                   Schedule, \
+                   MedicalHistory, \
+                   DeseaseRecipe, \
+                   DoctorVisit, \
+                   WorkTime
 
-# Register your models here.
+@admin.register(Doctor)
+class DoctorAdmin(admin.ModelAdmin):
+    """Doctor Admin Model"""
+    pass
+
+@admin.register(Patient)
+class PatientAdmin(admin.ModelAdmin):
+    """Patient Admin Model"""
+    pass
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    """Department Admin Model"""
+    pass
+                    
+@admin.register(Specialization)
+class SpecializationAdmin(admin.ModelAdmin):
+    """Specialization Admin Model"""
+    pass
+
+@admin.register(Chart)
+class ChartAdmin(admin.ModelAdmin):
+    """Chart Admin Model"""
+    pass
+            
+@admin.register(Schedule)
+class ScheduleAdmin(admin.ModelAdmin):
+    """Schedule Admin Model"""
+    pass
+
+@admin.register(MedicalHistory)
+class MedicalHistoryAdmin(admin.ModelAdmin):
+    """Medical History Admin Model"""
+    pass
+
+@admin.register(DeseaseRecipe)
+class DeseaseRecipeAdmin(admin.ModelAdmin):
+    """Desease Recipe Admin Model"""
+    pass
+
+@admin.register(DoctorVisit)
+class DoctorVisitAdmin(admin.ModelAdmin):
+    """Doctor Visit Admin Model"""
+    pass
+
+@admin.register(WorkTime)
+class WorkTimeAdmin(admin.ModelAdmin):
+    """Work Time Admin Model"""
+    pass
