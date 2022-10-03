3e53574774591869558466efd6f15e5f9ae8642e
korchizhinskiy <korchizhinskiy@gmail.com>
2022-10-03 13:50:47 +0300

    Add admins classes with created models

--- database/admin.py
+++ database/admin.py
@@ -1,43 +1,61 @@
 from django.contrib import admin
-from .models import Doctor, \
-                   Patient, \
-                   Department, \
-                   Specialization, \
-                   Chart, \
-                   Schedule, \
-                   MedicalHistory, \
-                   DeseaseRecipe, \
-                   DoctorVisit, \
-                   WorkTime
+from .models import Department, \
+                    Specialization, \
+                    Day, \
+                    WorkTime, \
+                    Chart, \
+                    Doctor, \
+                    Schedule,\
+                    Recipe, \
+                    Desease, \
+                    MedicalHistory, \
+                    Patient, \
+                    Visit
 
-@admin.register(Doctor)
-class DoctorAdmin(admin.ModelAdmin):
-    """Doctor Admin Model"""
-    pass
-
-@admin.register(Patient)
-class PatientAdmin(admin.ModelAdmin):
-    """Patient Admin Model"""
-    pass
 
 @admin.register(Department)
 class DepartmentAdmin(admin.ModelAdmin):
     """Department Admin Model"""
     pass
-                    
+
 @admin.register(Specialization)
 class SpecializationAdmin(admin.ModelAdmin):
     """Specialization Admin Model"""
+    pass
+
+@admin.register(Day)
+class DayAdmin(admin.ModelAdmin):
+    """Day Admin Model"""
+    pass
+
+@admin.register(WorkTime)
+class WorkTimeAdmin(admin.ModelAdmin):
+    """Work Time Admin Model"""
     pass
 
 @admin.register(Chart)
 class ChartAdmin(admin.ModelAdmin):
     """Chart Admin Model"""
     pass
-            
+
+@admin.register(Doctor)
+class DoctorAdmin(admin.ModelAdmin):
+    """Specialization Admin Model"""
+    pass
+
 @admin.register(Schedule)
 class ScheduleAdmin(admin.ModelAdmin):
-    """Schedule Admin Model"""
+    """Specialization Admin Model"""
+    pass
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    """Recipe Admin Model"""
+    pass
+
+@admin.register(Desease)
+class DeseaseAdmin(admin.ModelAdmin):
+    """Desease Admin Model"""
     pass
 
 @admin.register(MedicalHistory)
@@ -45,17 +63,12 @@ class MedicalHistoryAdmin(admin.ModelAdmin):
     """Medical History Admin Model"""
     pass
 
-@admin.register(DeseaseRecipe)
-class DeseaseRecipeAdmin(admin.ModelAdmin):
-    """Desease Recipe Admin Model"""
+@admin.register(Patient)
+class PatientAdmin(admin.ModelAdmin):
+    """Patient Admin Model"""
     pass
 
-@admin.register(DoctorVisit)
-class DoctorVisitAdmin(admin.ModelAdmin):
-    """Doctor Visit Admin Model"""
-    pass
-
-@admin.register(WorkTime)
-class WorkTimeAdmin(admin.ModelAdmin):
-    """Work Time Admin Model"""
+@admin.register(Visit)
+class VisitAdmin(admin.ModelAdmin):
+    """Visit Admin Model"""
     pass
