e9e6eaf52ce5c8fe70209d2791daf62e2bba603d
korchizhinskiy <korchizhinskiy@gmail.com>
2022-10-15 12:46:32 +0300

    Add list and detail views for patient and doctor

--- database/views.py
+++ database/views.py
@@ -1,5 +1,44 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
-def home_page(requests):
-    return HttpResponse('Ответ')
+from .models import Doctor, Patient
+
+class DoctorsListView(ListView):
+    """Doctor List View"""
+    model = Doctor
+    allow_empty = False
+    template_name = 'database/doctors_list.html'
+    context_object_name = 'doctors'
+
+    def get_queryset(self):
+        """Get all Doctors with their departments and specializations"""
+        doctors = Doctor.objects.select_related(
+                                                'doctor_department_id', 
+                                                'doctor_specialization_id'
+                                                ).all()
+        return doctors
+
+class DoctorDetailView(DetailView):
+    """Doctor Detail View"""
+    model = Doctor
+    context_object_name = 'doctor'
+    template_name = 'database/doctor.html'
+
+class PatientsListView(ListView):
+    """Patient List View"""
+    model = Patient
+    allow_empty = False
+    template_name = 'database/patients_list.html'
+    context_object_name = 'patients'
+
+    def get_queryset(self):
+        """Get all Patients """
+        patients = Patient.objects.order_by('patient_last_name', 
+                                            'patient_first_name').all()
+        return patients
+
+class PatientDetailView(DetailView):
+    """Patient Detail View"""
+    model = Patient
+    context_object_name = 'patient'
+    template_name = 'database/patient.html'
