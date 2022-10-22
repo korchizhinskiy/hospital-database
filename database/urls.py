fb5f8b195ad72238642a45cf600cffa60fedd771
korchizhinskiy <korchizhinskiy@gmail.com>
2022-10-22 14:49:04 +0300

    Add new path for hame page

--- database/urls.py
+++ database/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from .views import DoctorDetailView, \
                    DoctorsListView, \
                    PatientDetailView, \
-                   PatientsListView
+                   PatientsListView, \
+                   home_page
 
 urlpatterns = [
+        path('', home_page, name='home'),
         path('doctors_list/', DoctorsListView.as_view(), name='doctors_list'),
         path('patients_list/', PatientsListView.as_view(), name='patients_list'),
         path('patient/<int:pk>', PatientDetailView.as_view(), name='patient'),
