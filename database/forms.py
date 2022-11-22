f0c65840a4f6954900592cb1da9f1993a02d9734
korchizhinskiy <korchizhinskiy@gmail.com>
2022-11-22 13:00:33 +0300

    Cleaning variables in form

--- database/forms.py
+++ database/forms.py
@@ -1,6 +1,10 @@
+from django.core.exceptions import ValidationError
 from django.forms import ModelForm
 from django.forms.widgets import DateTimeInput
 from .models import Visit
+
+from .utils import get_doctor_worktime
+
 
 
 class VisitForm(ModelForm):
@@ -15,4 +19,18 @@ class VisitForm(ModelForm):
         widgets = {
                 'visit_datetime': DateTimeInput(attrs={'type': 'datetime-local'})
                 }
-    
+
+    #TODO: Check day of week
+    def clean_visit_datetime(self):
+        """Validate the DateTime of the appointment to the doctor"""
+        doctor_id = self.cleaned_data['visit_doctor_id']
+        input_datetime = self.cleaned_data['visit_datetime']
+
+        
+        worktime = get_doctor_worktime(doctor_id)
+
+        if not worktime['doctor_worktime_start'] <= input_datetime.time() <= worktime['doctor_worktime_end']:
+            raise ValidationError("В это время врач не работает")
+
+        return input_datetime
+
