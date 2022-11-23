e0f8af4b334d7608b7e90610f04392ac64f796d5
korchizhinskiy <korchizhinskiy@gmail.com>
2022-11-23 03:18:29 +0300

    Add Router to DoctorViewSet
    Include DoctorViewSet with Simple Router by path 'api/'

--- database/urls.py
+++ database/urls.py
@@ -1,20 +1,13 @@
-from django.urls import path
-from .views import DoctorDetailView, \
-                   DoctorsListView, \
-                   PatientDetailView, \
-                   PatientsListView, \
-                   VisitDetailView, \
-                   home_page, \
-                   visit_page
+from django.urls import include, path
+from rest_framework import routers
+
+from database.views import DoctorViewSet
+
+
+router = routers.SimpleRouter()
+router.register(r'doctor', DoctorViewSet, basename='doctor')
 
 
 urlpatterns = [
-        path('', home_page, name='home'),
-        path('doctors_list/', DoctorsListView.as_view(), name='doctors_list'),
-        path('patients_list/', PatientsListView.as_view(), name='patients_list'),
-        path('patient/<int:pk>', PatientDetailView.as_view(), name='patient'),
-        path('doctor/<int:pk>', DoctorDetailView.as_view(), name='doctor'),
-        path('visit/<int:pk>', VisitDetailView.as_view(), name='visit'),
-        path('get_visit/', visit_page, name='get_visit'),
-
+    path('api/', include(router.urls))
 ]
