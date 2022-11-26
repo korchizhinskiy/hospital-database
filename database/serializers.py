e191e178234443101eec7ead1e3ca4f778af989c
korchizhinskiy <korchizhinskiy@gmail.com>
2022-11-26 07:21:23 +0300

    Add new serializer for models
    List of change:
     - Change formatting of imports;
     - Add serializers for this models:
      * Specialization;
      * Department;
      * Doctor;
      * Desease;
      * MedicalHistory;
      * Patient;
      * Visit.
     - Change fields in include serializers.

--- database/serializers.py
+++ database/serializers.py
@@ -1,12 +1,18 @@
 from rest_framework import serializers
 
-from database.models import Department, \
+from database.models import Chart, \
+                            Department, \
+                            Desease, \
                             Doctor, \
-                            Specialization
+                            MedicalHistory, \
+                            Patient, \
+                            Specialization, \
+                            Visit
 
                             
 
 class SpecializationSerializer(serializers.ModelSerializer):
+    """Specialization Serializer"""
 
     class Meta:
         model = Specialization
@@ -16,6 +22,7 @@ class SpecializationSerializer(serializers.ModelSerializer):
 
 
 class DepartmentSerializer(serializers.ModelSerializer):
+    """Department Serializer"""
 
     class Meta:
         model = Department
@@ -23,12 +30,24 @@ class DepartmentSerializer(serializers.ModelSerializer):
                 "department_name",
                 )
         
+
+class ChartSerializer(serializers.ModelSerializer):
+    """Chart Serializer"""
+    
+    class Meta:
+        model = Chart
+        fields = (
+                "chart_day_id",
+                "chart_work_time_start",
+                "chart_work_time_end",
+                )
     
 
 class DoctorSerializer(serializers.ModelSerializer):
     """Doctor Serializer"""
     doctor_specialization_id = SpecializationSerializer()
     doctor_department_id = DepartmentSerializer()
+    doctor_chart = ChartSerializer(source="chart_set", many=True)
     
     class Meta:
         model = Doctor
@@ -39,5 +58,59 @@ class DoctorSerializer(serializers.ModelSerializer):
                 "doctor_age",
                 "doctor_department_id",
                 "doctor_specialization_id",
+                "doctor_chart"
                 )
-        
+
+
+class DeseaseSerializer(serializers.ModelSerializer):
+    """Desease Serializer"""
+
+    class Meta:
+        model = Desease
+        fields = (
+                "desease_name",
+                "recipe_text",
+                )
+    
+
+class MedicalHistorySerializer(serializers.ModelSerializer):
+    """Medical History Serializer"""
+    medical_history_desease_id = DeseaseSerializer()
+
+    class Meta:
+        model = MedicalHistory
+        fields = (
+                "medical_history_start",
+                "medical_history_end",
+                "medical_history_desease_id",
+                )
+
+
+class PatientSerializer(serializers.ModelSerializer):
+    """Patient Serializer"""
+    medical_history = MedicalHistorySerializer(source="medicalhistory_set", many=True)
+
+    class Meta:
+        model = Patient
+        fields = (
+                "patient_last_name",
+                "patient_first_name",
+                "patient_second_name",
+                "patient_age",
+                "medical_history",
+                )
+
+
+class VisitSerializer(serializers.ModelSerializer):
+    """Visit Serializer"""
+    visit_doctor_id = DoctorSerializer()
+    visit_patient_id = PatientSerializer()
+
+    class Meta:
+        model = Visit
+        fields = (
+                "visit_doctor_id",
+                "visit_patient_id",
+                "visit_datetime",
+                )
+    
