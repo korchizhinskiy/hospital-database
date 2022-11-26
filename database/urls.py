c099d3543cdf976d61301b08b8ee6d7f72cd7067
korchizhinskiy <korchizhinskiy@gmail.com>
2022-11-26 07:25:27 +0300

    Register new patterns in router
    Add new path for patient and visit with them ViewSets

--- database/urls.py
+++ database/urls.py
@@ -1,11 +1,16 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from database.views import DoctorViewSet
+from database.views import DoctorViewSet, \
+                           PatientViewSet, \
+                           VisitViewSet
+
 
 
 router = routers.SimpleRouter()
 router.register(r'doctor', DoctorViewSet, basename='doctor')
+router.register(r'patient', PatientViewSet, basename='patient')
+router.register(r'visit', VisitViewSet, basename='visit')
 
 
 urlpatterns = [
